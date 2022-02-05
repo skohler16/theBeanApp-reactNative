590f83654d35eeb87405f74ca380d57cd0a6133d
Scott Kohler <skohler16@gmail.com>
2022-02-04 21:22:55 -0700

    added tab navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,97 +2,23 @@ import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import LocationInfo from "./LocationInfoComponents";
-import Contact from "./ContactComponent";
-import About from "./AboutComponent";
-import Constants from "expo-constants";
 import { Platform, View } from "react-native";
-import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createAppContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
+import Constants from "expo-constants";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
-const DirectoryNavigator = createStackNavigator(
-  {
-    Directory: { screen: Directory },
-    LocationInfo: { screen: LocationInfo },
-  },
-  {
-    initialRouteName: "Directory",
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "#331810",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        color: "#fff",
-      },
-    },
-  }
-);
+const Tab = createMaterialBottomTabNavigator();
 
-const HomeNavigator = createStackNavigator(
-  {
-    Home: { screen: Home },
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "#331810",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        color: "#fff",
-      },
-    },
-  }
-);
-
-const AboutNavigator = createStackNavigator(
-  {
-    About: { screen: About },
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "#331810",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        color: "#fff",
-      },
-    },
-  }
-);
-
-const ContactNavigator = createStackNavigator(
-  {
-    Contact: { screen: Contact },
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "#331810",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        color: "#fff",
-      },
-    },
-  }
-);
-
-const MainNavigator = createDrawerNavigator(
-  {
-    Home: { screen: HomeNavigator },
-    Directory: { screen: DirectoryNavigator },
-    AboutNavigator: { screen: AboutNavigator },
-    ContactNavigator: { screen: ContactNavigator },
-  },
-  {
-    drawerBackgroundColor: "#B29779",
-  }
-);
-
-const AppNavigator = createAppContainer(MainNavigator);
+function BottomTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Locations" component={Directory} onPress={} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 class Main extends Component {
   render() {
@@ -103,7 +29,7 @@ class Main extends Component {
           paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
         }}
       >
-        <AppNavigator />
+        <BottomTabs />
       </View>
     );
   }
