fbfe43a73e3d976ad0483885cda490b94bb02ed4
Scott Kohler <skohler16@gmail.com>
2022-02-10 21:20:22 -0700

    fixed contact info component

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,38 +1,22 @@
 import React, { Component } from "react";
 import { ScrollView } from "react-native-gesture-handler";
-import { Text, Card, Button, Icon } from "react-native";
+import { Text, Card } from "react-native-elements";
 
 class Contact extends Component {
   static navigationOptions = {
     title: "Contact Us",
   };
 
-  sendMail() {
-    MailComposer.composeAsync({
-      recipients: ["thebeanroasters@gmail.com"],
-      subject: "Inquiry",
-      body: "To whom it may concern:",
-    });
-  }
-
   render() {
     return (
       <ScrollView>
-        <Card title={"Contact Info"}>
+        <Card title={"The Bean Coffee House"} wrapperStyle={{ margin: 20 }}>
           <Text>Phone: 575-527-5155</Text>
-          <Button
-            title="Send Email"
-            buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
-            icon={
-              <Icon
-                name="envelope-o"
-                type="font-awesome"
-                color="#fff"
-                iconStyle={{ marginRight: 10 }}
-              />
-            }
-            onPress={() => this.sendMail()}
-          />
+          <Text>Email: thebeanroasters@gmail.com</Text>
+        </Card>
+        <Card title={"The Bean at Josephina's"} wrapperStyle={{ margin: 20 }}>
+          <Text>Phone: 575-555-5555</Text>
+          <Text>Email: thebeanjosiXXX@gmail.com</Text>
         </Card>
       </ScrollView>
     );
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -16,15 +16,7 @@ const Stack = createNativeStackNavigator();
 function StackedLocation() {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Directory"
-        component={Directory}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
+      <Stack.Screen name="Directory" component={Directory} />
       <Stack.Screen name="Locations" component={LocationInfo} />
     </Stack.Navigator>
   );
